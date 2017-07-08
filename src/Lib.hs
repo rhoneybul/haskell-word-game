f8d8b0e8554872101266c2f2bca855c4fc947eb3
rthdatasolutions <robert.honeybul@rthdatasolutions.com>
2017-07-08 12:44:20 +0800

    added tests and diagonlize

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,14 +1,14 @@
 module Lib
-    ( grid
-    , languages
-    , formatGrid
+    ( formatGrid
     , outputGrid
     , findWord
     , findWords
     , findWordInLine
-	) where
+    , skew 
+    , diagonalize
+    ) where
 
-import Data.List (isInfixOf)
+import Data.List (isInfixOf, transpose)
 import Data.Maybe (catMaybes)
 
 type Grid = [String]
@@ -20,39 +20,34 @@ formatGrid :: Grid -> String
 formatGrid = unlines
 
 findWords :: Grid -> [String] -> [String]
-findWords grid words = catMaybes (map (findWord grid) words)
+findWords grid words = 
+    let gridLines = getLines grid 
+    in catMaybes (map (findWord gridLines) words)
+
+getLines :: Grid -> [String]
+getLines grid =
+    let horizontal = grid
+        vertical = transpose grid 
+        diagonal1 = diagonalize grid
+        diagonal2 = diagonalize (map reverse grid)
+        lines = horizontal ++ vertical ++ diagonal1 ++ diagonal2
+    in lines ++ (map reverse lines)
+
+diagonalize :: Grid -> Grid 
+diagonalize = (transpose . skew) 
+
+skew :: Grid -> Grid 
+skew [] = []
+skew (l:ls) = l : skew ( map indent ls)
+    where indent line = '_':line
 
 findWord :: Grid -> String -> Maybe String 
-findWord grid word =
-	let lines = grid ++ map reverse grid 
-	    found = or $ map (findWordInLine word) lines 
-	in if found then Just word else Nothing
+findWord grid word = 
+    let lines = getLines grid
+        found = or $ map (findWordInLine word) lines
+    in if found then Just word else Nothing 
 
 findWordInLine :: String -> String -> Bool
 findWordInLine word line = word `isInfixOf` line
 
-grid = [  "__C_________R__"
-		, "__SI________U__"
-		, "__HASKELL____B_"
-		, "__A__A_____S__Y"
-		, "__R___B___C____"
-		, "__PHP____H_____"
-		, "____S_LREP_____"
-		, "____I__M_Y__L__"
-		, "____L_E__T_O___"
-		, "_________HB____"
-		, "_________O_____"
-		, "________CN_____"
-		]
 
-languages = [  "BASIC"
-			,  "COBOL"
-			,  "CSHARP"
-			,  "HASKELL"
-			,  "LISP"
-			,  "PERL"
-			,  "PHP"
-			,  "PYTHON"
-			,  "RUBY"
-			,  "SCHEME"
-			]
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,20 @@
-Use Test.Hspec
+import Test.Hspec
 import Lib
+import Data
 
+main :: IO ()
 main = hspec $ do
-	describe "How to run a test" $ do 
-		it "Should be able to run a test" $ do
-			someString `shouldBe` "Hello, Words."
+    describe "formatGrid" $ do 
+        it "Should concatenate every line with new line" $ do
+            (formatGrid ["abc", "def", "ghi"]) `shouldBe` "abc\ndef\nghi\n"
 
+    describe "findWord" $ do
+        it "Should find words that exist on the grid" $ do 
+            (findWord grid "HASKELL") `shouldBe` Just "HASKELL"
+            (findWord grid "CSHARP") `shouldBe` Just "CSHARP"
+        it "Should return nothing for words not on the grid" $ do 
+            (findWord grid "HAMSTER") `shouldBe` Nothing
+
+    describe "findWords" $ do
+        it "Should find all languages on the grid" $ do 
+            (findWords grid languages ) `shouldBe` languages 
